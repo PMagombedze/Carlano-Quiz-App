ebfd589f53bc44ea0d5900e03c5b8f26b56d3d0a
PMagombedze <percymagoras@outlook.com>
2024-03-25 21:28:47 +0200

    generate unique question id

--- website/api/add_or_update.py
+++ website/api/add_or_update.py
@@ -1,13 +1,24 @@
 import requests
 from requests.auth import HTTPBasicAuth
+import time
+
+counter = 0
+
+def generate_unique_id():
+    global counter
+    timestamp = int(time.time() * 1000)  # Get current timestamp in milliseconds
+    counter += 1
+    unique_id = (timestamp << 16) | (counter % 65536)  # Combine timestamp and counter
+    return unique_id
 
 item_data = {
-        "text": "Which of the following is a built-in module in Python for working with dates and times?",
-        "options": [["datetime", "math", "os", "random"]],
-        "correct_answer": "datetime"
-    }
+        "text": "What is the output of the following code?\n\nx = 10\ny = 5\nprint(x > y)",
+        "options": [["True", "False", "Error", "None"]],
+        "correct_answer": "True"
+}
 
-response = requests.put(f'http://localhost:5000/api/v1/1', json=item_data, auth=HTTPBasicAuth('05992ccec183b09f19354ba55014c19b','6e9ffaebace7cb744324c0e8784a2c69'))
+myId = generate_unique_id()
+response = requests.put(f'http://localhost:5000/api/v1/{myId}', json=item_data, auth=HTTPBasicAuth('05992ccec183b09f19354ba55014c19b','6e9ffaebace7cb744324c0e8784a2c69'))
 
 if response.status_code == 201:
     item = response.json()
--- website/api/delete.py
+++ website/api/delete.py
@@ -2,7 +2,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 
-response = requests.delete('http://localhost:5000/api/v1/3', auth=HTTPBasicAuth('percy_magom','6e9ffaebace7cb744324c0e8784a2c69'))
+response = requests.delete('http://localhost:5000/api/v1/0', auth=HTTPBasicAuth('05992ccec183b09f19354ba55014c19b','6e9ffaebace7cb744324c0e8784a2c69'))
 
 if response.status_code == 200:
     print('Quiz deleted successfully')
